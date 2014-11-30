f24e0396fe841a0e8b29be10e59011288a42e1dc
georgeoblapenko <kunstmord@gmail.com>
2014-11-30 21:47:20 +0300

    some basic serializers done

--- abandoned/serializers.py
+++ abandoned/serializers.py
@@ -1,3 +1,32 @@
 __author__ = 'georgeoblapenko'
 
 from rest_framework import serializers
+from abandoned.models import Author, Reason, Tag, Project
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('id', 'name', 'link',)
+
+
+class ReasonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reason
+        fields = ('id', 'reason',)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'tag',)
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    author = serializers.RelatedField()
+    reason = serializers.RelatedField()
+    tags = serializers.RelatedField(many=True)
+
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'link', 'author', 'description', 'reason', 'tags', 'upvotes', 'downvotes', 'date_added')
