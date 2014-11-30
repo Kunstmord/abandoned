c68f737e5648b6625517f87c8edc51adbe4c4366
georgeoblapenko <kunstmord@gmail.com>
2014-11-30 21:47:11 +0300

    models finished, it seems?

--- abandoned/models.py
+++ abandoned/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 
 
-class Project(models.Model):
-    name = models.CharField(max_length=100)
-    reason = models.TextField()
-    author_link = models.URLField()
+class Author(models.Model):
+    name = models.CharField(max_length=200)
     link = models.URLField()
-    upvotes = models.IntegerField()
-    downvotes = models.IntegerField()
+
+
+class Reason(models.Model):
+    reason = models.CharField(max_length=200)
 
 
 class Tag(models.Model):
     text = models.CharField(max_length=100)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=400)
+    link = models.URLField()
+    author = models.ForeignKey(Author)
+    description = models.TextField()
+    reason = models.ForeignKey(Reason)
+    tags = models.ManyToManyField(Tag)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    date_added = models.DateTimeField()
