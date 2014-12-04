7e85ddb95562e698dbbf8c7ecd93dfd322eb49a6
georgeoblapenko <kunstmord@gmail.com>
2014-12-04 18:05:56 +0300

    API viewsets added

--- abandoned/views.py
+++ abandoned/views.py
@@ -1,1 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from abandoned.models import Project, Tag, Author, Reason
+from abandoned.serializers import TagSerializer, ProjectSerializer, AuthorSerializer, ReasonSerializer
+
+
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class ReasonViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Reason.objects.all()
+    serializer_class = ReasonSerializer
+
+
+class ProjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
