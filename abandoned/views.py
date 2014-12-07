3aa865ef1bd7b374ec700750dc2cd56ae9ce3867
georgeoblapenko <kunstmord@gmail.com>
2014-12-07 15:58:52 +0300

    new Tag serializer, ordering of authors by total votes

--- abandoned/views.py
+++ abandoned/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
-from django.db.models import Count
+from django.db.models import Count, Sum
 from rest_framework import viewsets
 from abandoned.models import Project, Tag, Author, Reason
 from rest_framework.response import Response
 from rest_framework.decorators import list_route
-from abandoned.serializers import BaseTagSerializer, ProjectSerializer, ReasonSerializer, AuthorSerializer
+from abandoned.serializers import TagSerializer, ProjectSerializer, ReasonSerializer, AuthorSerializer
 
 
 class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-    # @list_route()
-    # def votes(self, request):
-    #     votes = Author.objects.annotate()
-    #     top = Project.objects.all().order_by('upvotes').reverse()
-    #     page = self.paginate_queryset(top)
-    #     serializer = self.get_pagination_serializer(page)
-    #     return Response(serializer.data)
+    @list_route()
+    def votes(self, request):
+        votes = Author.objects.annotate(votes_total=Sum('projects__upvotes')).order_by('votes_total').reverse()
+        page = self.paginate_queryset(votes)
+        serializer = self.get_pagination_serializer(page)
+        return Response(serializer.data)
 
     @list_route()
     def projects(self, request):
@@ -29,7 +28,7 @@ class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
-    serializer_class = BaseTagSerializer
+    serializer_class = TagSerializer
 
 
 class ReasonViewSet(viewsets.ReadOnlyModelViewSet):
