ea592044aa1d0743403d5552577fd8f97e70af11
georgeoblapenko <kunstmord@gmail.com>
2014-12-20 20:24:04 +0300

    handle_submit url added

--- abandonedio/urls.py
+++ abandonedio/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
-from abandoned.views import TagViewSet, AuthorViewSet, ProjectViewSet, ReasonViewSet
+from abandoned.views import TagViewSet, AuthorViewSet, ProjectViewSet, ReasonViewSet, LanguageViewSet, handle_submit
 
 
 router = routers.DefaultRouter()
@@ -9,6 +9,7 @@ router.register(r'api/tags', TagViewSet)
 router.register(r'api/authors', AuthorViewSet)
 router.register(r'api/reasons', ReasonViewSet)
 router.register(r'api/projects', ProjectViewSet)
+router.register(r'api/languages', LanguageViewSet)
 
 
 urlpatterns = patterns('',
@@ -16,5 +17,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'abandonedio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include(router.urls)),
+    url(r'^api/submit/', handle_submit),
     url(r'^admin/', include(admin.site.urls)),
 )
