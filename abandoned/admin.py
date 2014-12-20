e132fc45d6c2be37a52bb46d46b401b195e4a5d6
georgeoblapenko <kunstmord@gmail.com>
2014-12-20 20:24:30 +0300

    registered Language model

--- abandoned/admin.py
+++ abandoned/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from abandoned.models import Tag, Project, Author, Reason
+from abandoned.models import Tag, Project, Author, Reason, Language
 
 admin.site.register(Tag)
 admin.site.register(Project)
 admin.site.register(Author)
 admin.site.register(Reason)
+admin.site.register(Language)
