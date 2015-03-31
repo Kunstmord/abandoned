c5c33faaa86e28ec916f126625298cdfcf8df83e
georgeoblapenko <kunstmord@gmail.com>
2015-03-31 21:34:51 +0300

    new settings for the Django Rest Framework Pagination API

--- abandonedio/settings.py
+++ abandonedio/settings.py
@@ -55,9 +55,8 @@ ROOT_URLCONF = 'abandonedio.urls'
 WSGI_APPLICATION = 'abandonedio.wsgi.application'
 
 REST_FRAMEWORK = {
-    'PAGINATE_BY': 20,                 # Default to 10
-    'PAGINATE_BY_PARAM': 'page_size',  # Allow client to override, using `?page_size=xxx`.
-    'MAX_PAGINATE_BY': 100             # Maximum limit allowed when using `?page_size=xxx`.
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'
 }
 
 # Database
